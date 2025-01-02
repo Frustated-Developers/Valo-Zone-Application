f02b26730e93ffa25dcec831ddb6c77ad89bbe2d
yesra29 <147398141+yesra29@users.noreply.github.com>
2025-01-02 22:12:05 +0530

    login page modified

--- lib/login/view/loginPage.dart
+++ lib/login/view/loginPage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:valo_zone/utils/Assets_path.dart';
 import 'package:valo_zone/utils/reusable_widgets/CustomButton.dart/CustomButton.dart';
 
